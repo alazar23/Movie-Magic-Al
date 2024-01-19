e48b3cdc5b4f257e6c3aa20abf52bc01afafa364
alazar23 <145958466+alazar23@users.noreply.github.com>
2024-01-19 16:35:01 +0200

    add create movie page

---
+++ src/controllers/createController.js
@@ -0,0 +1,15 @@
+const router = require('express').Router()
+
+
+router.get('/create',(req,res) =>{
+    res.render('create')
+})
+
+
+
+
+
+
+
+
+module.exports = router
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,17 @@
 const router = require('express').Router()
 
 const homeController = require('./controllers/homeController')
+const movieController = require('./controllers/createController')
+
+
+
 
 router.use(homeController)
+router.use(movieController)
+
+
+
+
+
 
 module.exports = router
