ec819310ebccd84b7216f69c609659631a682682
alazar23 <145958466+alazar23@users.noreply.github.com>
2024-01-19 16:20:36 +0200

    extract routes and add home controller

---
+++ src/controllers/homeController.js
@@ -0,0 +1,13 @@
+const router = require('express').Router()
+
+
+router.get('/',(req,res) =>{
+    res.render('home',{layout:false})
+})
+
+
+
+
+
+
+module.exports = router
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const handlebars = require('express-handlebars')
 const path = require('path')
 
+const router = require('./routes')
+
 const app = express()
 const PORT = 5000
 
@@ -14,10 +16,7 @@ app.set('views',path.join(__dirname,'views'))
 
 app.use(express.static(path.join(__dirname,'public')))
 
-app.get('/',(req,res) =>{
-    res.render('home',{layout:false})
-})
-
+app.use(router)
 
 
 
