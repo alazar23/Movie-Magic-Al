9bbba21f6fd35d8eaabf7846feda1886f5bd5e90
alazar23 <145958466+alazar23@users.noreply.github.com>
2024-01-21 11:25:31 +0200

    add hardcoded movie

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,4 +1,13 @@
-exports.create = (movieData) =>{
 
-    console.log(movieData);
+const movies = [{
+    title: 'Suits',
+    genre: 'Goten',
+    director: 'az',
+    year: '2020',
+    imageUrl: 'nqma',
+    rating: '5',
+    description: 'mnogo top film'
+  }]
+exports.create = (movieData) =>{
+    movies.push(movieData)
 }
