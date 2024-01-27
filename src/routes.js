519bc55b2c9b47f04006c219e60c8febc85bea80
alazar23 <145958466+alazar23@users.noreply.github.com>
2024-01-27 19:43:52 +0200

    add created cast page!

--- src/routes.js
+++ src/routes.js
@@ -2,16 +2,17 @@ const router = require('express').Router()
 
 const homeController = require('./controllers/homeController')
 const movieController = require('./controllers/createController')
+const castController = require('./controllers/castCotroller')
 
 
 
 
 router.use(movieController)
-
-router.use(homeController)
-
+router.use('/cast',castController)
 
 
 
-
+//tozi kontroler vinagi trqbva da e posleden glupako!!!
+router.use(homeController) //<-----------------------------!!!
+//ne go pipai!!!! 
 module.exports = router
